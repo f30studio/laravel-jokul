85f651f4a8e65a8db5c4ce0ee852ef598f44af34
fathalfath30 <fathalfath30@gmail.com>
2022-02-12 03:14:04 +0700

    feat: adding basic configuration file

---
+++ config/jokul.php
@@ -0,0 +1,23 @@
+<?php
+/*
+//  ______ ____   ___   _____ _             _ _
+// |  ____|___ \ / _ \ / ____| |           | (_)
+// | |__    __) | | | | (___ | |_ _   _  __| |_  ___
+// |  __|  |__ <| | | |\___ \| __| | | |/ _` | |/ _ \
+// | |     ___) | |_| |____) | |_| |_| | (_| | | (_) |
+// |_|    |____/ \___/|_____/ \__|\__,_|\__,_|_|\___/
+//
+//
+// Written by Fathalfath30.
+// Email : fathalfath30@gmail.com
+// Follow me on:
+//  Github : https://github.com/fathalfath30
+//  Gitlab : https://gitlab.com/Fathalfath30
+*/
+
+return [
+  'payment' => [
+    'client_id' => env('JOKUL_CLIENT_ID', null),
+    'secret_key' => env('JOKUL_SECRET_KEY', null)
+  ]
+];
